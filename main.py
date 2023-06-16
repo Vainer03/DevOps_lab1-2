9ef6277bf88bace3de36adf5039b9cd2a267cc08
Anastasiia <106194054+Vainer03@users.noreply.github.com>
2023-06-16 16:57:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 def calc(a, b, operator):
-  match operator:
-    case "+":
-        result = a + b
-    case "-":
-        result = a - b
-    case "*":
-        result = a * b
-    case "/":
-        if b == 0:
-          raise ZeroDivisionError
-        else:
-          result = a/b
-    case _:
-        raise ValueError
-  return result
+    match operator:
+        case "+":
+            result = a + b
+        case "-":
+            result = a - b
+        case "*":
+            result = a * b
+        case "/":
+            if b == 0:
+                raise ZeroDivisionError
+            else:
+                result = a / b
+        case _:
+            raise ValueError
+    return result
+
 
 def main():
-  try:
-    a = float(input())
-    b = float(input())
-  except:
-    print("both a and b are supposed to be numbers")
-    return 1
-  
-  operator = input()
-  try:
-    print(calc(a, b, operator))
-  except ZeroDivisionError:
-    print("Division by zero is not allowed in here")
-    return 2
-  except ValueError:
-      print("Unknown query")
-      return 3
-  return 0
+    try:
+        a = float(input())
+        b = float(input())
+    except ValueError:
+        print("both a and b are supposed to be numbers")
+        return 1
 
-main()
+    operator = input()
+    try:
+        print(calc(a, b, operator))
+    except ZeroDivisionError:
+        print("Division by zero is not allowed in here")
+        return 2
+    except ValueError:
+        print("Unknown query")
+        return 3
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+    
